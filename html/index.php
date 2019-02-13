84cf6004417ccee037b0b67f04d708a88d4bec2d
qqqlab <qqqlab@users.noreply.github.com>
2019-02-13 14:30:20 +0100

    graph

--- html/index.php
+++ html/index.php
@@ -10,7 +10,7 @@ if($interval<=0) $interval=300;
 //========================================================
 //graph
 //========================================================
-if(isset($_GET['go'])) {
+if($_GET) {
   //map topic name to topic column 
   $col = $_GET['t'];
   $c=0;
@@ -62,24 +62,50 @@ GROUP BY 1,2 ORDER BY 1
 //==================================================
 echo "<br><br><br><form name='form1'>";
 
-$res = $db->query("select distinct topic from log order by substring_index(topic,'/',-1),1");
-$i=0;
-echo "<table border=0><tr>";
-while($r = $res->fetch_row()) {
-  $topic = $r[0];
-  echo "<td>";
-  echo "<input name='t[$topic]' type='checkbox' " . (@$_GET['t'][$topic] ? 'checked' : '') . ">";
-  echo "$topic ";
-  echo "</td>";
-  $i++;
-  if($i%5==0) echo '</tr><tr>';
+if(!@$_GET['byval']) {
+  //group by node
+  $res = $db->query("select distinct topic from log order by 1");
+  $last_part1='';
+  echo "<table border=0><tr>";
+  while($r = $res->fetch_row()) {
+    $topic = $r[0];
+    $part1 = substr("$topic",0, strrpos($topic,'/')); //all exept last element
+    $part2 = substr("$topic", (strrpos($topic,'/') + 1)); //last element
+    if($last_part1 && $last_part1 != $part1) echo "</tr>";
+    if($last_part1 != $part1) echo "<td><b>$part1</b></td><td>";
+    echo "<input name='t[$topic]' type='checkbox' " . (@$_GET['t'][$topic] ? 'checked' : '') . ">";
+    echo "$part2 ";
+    $last_part1 = $part1;
+  }
+  echo "</tr></table>";
+}else{
+  //group by value
+  $res = $db->query("select distinct topic from log order by substring_index(topic,'/',-1),1");
+  $last_part2='';
+  echo "<table border=0><tr>";
+  while($r = $res->fetch_row()) {
+    $topic = $r[0];
+    $part1 = substr("$topic",0, strrpos($topic,'/')); //all exept last element
+    $part2 = substr("$topic", (strrpos($topic,'/') + 1)); //last element
+    if($last_part2 && $last_part2 != $part2) echo "</tr>";
+    if($last_part2 != $part2) echo "<td><b>$part2</b></td><td>";
+    echo "<input name='t[$topic]' type='checkbox' " . (@$_GET['t'][$topic] ? 'checked' : '') . ">";
+    echo "$part1 ";
+    $last_part2 = $part2;
+  }
+  echo "</tr></table>";
 }
-echo "</tr></table>";
+echo "<hr />";
 
+//interval
 echo "interval<input name='interval' value='$interval'> sec ";
-echo "<input type='submit' value='go' name='go'>";
-echo "</form>";
 
+//date range - TODO
+
+//go button
+echo "<input type='submit' value='go' name='go'>";
+
+//check all/uncheck all
 ?>
 <script type="text/javascript" language="javascript">// <![CDATA[
 function checkAll(formname, checktoggle)
@@ -97,6 +123,21 @@ function checkAll(formname, checktoggle)
 <a onclick="javascript:checkAll('form1', true);" href="javascript:void();">check all</a>
 <a onclick="javascript:checkAll('form1', false);" href="javascript:void();">uncheck all</a>
 <?php
+
+//group by value
+echo "<input name='byval' type='checkbox' " . (@$_GET['byval'] ? 'checked' : '') . " onChange='this.form.submit()'>group by value";
+echo "</form>";
+echo "<hr />";
+
+//some stats
+$rows = $db->query("select count(*) from log")->fetch_row()[0];
+$rows1h = $db->query("select count(*) from log where ts>current_timestamp-3600")->fetch_row()[0];
+$rows1d = $db->query("select count(*) from log where ts>current_timestamp-24*3600")->fetch_row()[0];
+$last = $db->query("select max(ts) from log")->fetch_row()[0];
+echo "stats: $rows rows, $rows1d rows/day, $rows1h rows/hour, last $last";
+
+
+
 
 
 //javascript timestamp is in milliseconds
